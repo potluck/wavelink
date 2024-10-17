33651fad60b81280e671900e10ecbf3fb0ecea6c
Potluck Mittal <potluck.mittal@gmail.com>
2024-10-17 14:11:09 -0400

    break out players

--- pages/[...players].tsx
+++ pages/[...players].tsx
@@ -1,7 +1,15 @@
 import { useRouter } from 'next/router'
 
 export default function Page() {
-  const router = useRouter()
-  console.log(router.query.players);
-  return <p>Players: {router.query.players?.toString()}</p>
+  const router = useRouter();
+  const players = !Array.isArray(router.query.players)? (router.query.players? [router.query.players] : []): router.query.players;
+  const player1 = players.length > 0 ? players[0] : "No player set";
+  const player2 = players.length > 1 ? players[1] : "No opponent set";
+
+  return  <div>
+    <p>
+    Welcome: {player1}
+    </p>
+    <p>Your opponent is: {player2}</p>
+  </div>
 }
