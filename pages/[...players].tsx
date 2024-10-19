d68948fa2a61d8bcc2ee5c3d08220293de122293
Potluck Mittal <potluck.mittal@gmail.com>
2024-10-18 23:55:46 -0400

    basic interaction

--- pages/[...players].tsx
+++ pages/[...players].tsx
@@ -1,15 +1,69 @@
 import { useRouter } from 'next/router'
+import React from 'react';
+import GameState from './components/GameState';
+
+export enum PlayerState {
+  NeedTeammate,
+  NoGame,
+  GameToPlay,
+  Playing,
+  Waiting
+}
+
 
 export default function Page() {
   const router = useRouter();
+  console.log("hi pots", router.query.players);
   const players = !Array.isArray(router.query.players)? (router.query.players? [router.query.players] : []): router.query.players;
   const player1 = players.length > 0 ? players[0] : "No player set";
-  const player2 = players.length > 1 ? players[1] : "No opponent set";
+  const player2 = players.length > 1 ? players[1] : "No teammate set";
 
-  return  <div>
-    <p>
-    Welcome: {player1}
-    </p>
-    <p>Your opponent is: {player2}</p>
+  const [playerState, setPlayerState] = React.useState(PlayerState.NeedTeammate);
+
+  if (playerState == PlayerState.NeedTeammate && players.length > 1) {
+    setPlayerState(PlayerState.NoGame);
+  }
+
+  // get rounds for pair
+
+
+  const [word1, setWord1] = React.useState("");
+  const [word2, setWord2] = React.useState("");
+
+  function startGame() {
+    if (playerState == PlayerState.NoGame) {
+      setPlayerState(PlayerState.Playing);
+      setWord1("Dog");
+      setWord2("Tree");
+    }
+  }
+
+  function submitAnswer(submission : string) {
+    setPlayerState(PlayerState.Waiting);
+    console.log(submission);
+  }
+
+  return  (
+  <div className={`grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}>
+    <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <ul className="list-inside text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+        <li className="mb-2">
+          Welcome: {player1}
+        </li>
+        <li className="mb-2">
+          You're playing with: {player2}
+        </li>
+
+        <GameState
+            playerState={playerState}
+            startGame={startGame}
+            word1={word1}
+            word2={word2}
+            submitAnswer={submitAnswer}
+        />
+
+    </ul>
+    </main>
   </div>
+  );
 }
---
+++ pages/components/GameState.tsx
@@ -0,0 +1,67 @@
+import { FormEvent, useState } from "react";
+import { PlayerState } from "../[...players]";
+
+type GameStateProps = {
+  playerState: PlayerState,
+  startGame: () => void,
+  word1: string,
+  word2: string,
+  submitAnswer: (submission: string) => void,
+}
+
+export default function GameState({
+  playerState,
+  startGame,
+  word1,
+  word2,
+  submitAnswer,
+} : GameStateProps) {
+
+  const [answer, setAnswer] = useState("");
+
+
+  function handleSubmit(e : FormEvent<HTMLFormElement>){
+    e.preventDefault();
+    submitAnswer(answer);
+  }
+
+  return (
+    <div>
+    { (playerState == PlayerState.NoGame ) &&
+      <button
+          className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+          onClick={startGame}
+      >
+        Start round
+      </button>
+  }
+  { (playerState == PlayerState.Playing ) &&
+    <div>
+      <li className="mb-2">
+        Your words are: {word1} and {word2}.
+      </li>
+      <form onSubmit={handleSubmit}>
+        <textarea
+          value={answer}
+          onChange={e => setAnswer(e.target.value)}
+          className={"block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"}
+          placeholder={"Input wavelink..."}
+        />
+        <button
+          type="submit"
+          className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  }
+  { (playerState == PlayerState.NeedTeammate ) &&
+      <li className="mb-2">Send your link to a friend to play.</li>
+  }
+  { (playerState == PlayerState.Waiting ) &&
+      <li className="mb-2">Waiting for your friend to complete this round.</li>
+  }
+  </div>
+  );
+}
