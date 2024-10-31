4f2fe3ef1613ee3bea70e18fedce8ff50b1bb1f9
Potluck Mittal <potluck.mittal@gmail.com>
2024-10-30 22:17:24 -0400

    meta tag...

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>Wavelink - a Pots production</title>
         <meta property="og:site_name" content="Wavelink"></meta>
-        <meta property="og:title" content="Wavelink - a game of connections"></meta>
+        <meta property="og:title" content="A game of connections"></meta>
         <meta property="og:description" content="Can you match your partner's link?"></meta>
       </Head>
       <Component {...pageProps} />
