78054d54053d96c6d77af030ff917419494d9db3
Potluck Mittal <potluck.mittal@gmail.com>
2024-10-30 22:11:04 -0400

    add title and meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Wavelink - a Pots production</title>
+        <meta property="og:site_name" content="Wavelink"></meta>
+        <meta property="og:title" content="A game of connections"></meta>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
