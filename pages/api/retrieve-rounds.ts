7d6096833ee1ef712e8fb0bdc9547c8892ac193e
Potluck Mittal <potluck.mittal@gmail.com>
2024-10-31 21:14:17 -0400

    order rounds

--- pages/api/retrieve-rounds.ts
+++ pages/api/retrieve-rounds.ts
@@ -8,6 +8,6 @@ export default async function handler(
   response: NextApiResponse,
 ) {
   const gameId = request.query.gameId as string;
-  const {rows} = await sql`SELECT r.*, p.word1, p.word2 FROM rounds r JOIN pairs p on r.pair_id = p.id where r.game_id = ${gameId};`;
+  const {rows} = await sql`SELECT r.*, p.word1, p.word2 FROM rounds r JOIN pairs p on r.pair_id = p.id where r.game_id = ${gameId} order by r.id asc;`;
   return response.status(200).json({ rows });
 }
