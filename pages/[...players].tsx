c6c1d26a9e712378a3293d140e39735277d8d550
Potluck Mittal <potluck.mittal@gmail.com>
2025-01-16 16:55:27 -0500

    link to help page

--- pages/[...players].tsx
+++ pages/[...players].tsx
@@ -349,6 +349,9 @@ export default function Page() {
             />
           )}
       </main>
+      <footer className="row-start-3 text-sm">
+        <a href="/help" className="text-blue-600 hover:text-blue-800 underline">How to play</a>
+      </footer>
     </div>
   );
 }
