bdefac3fbbc955b07e5decfe7dda9a1f260dc3dd
Potluck Mittal <potluck.mittal@gmail.com>
2025-01-16 16:23:31 -0500

    randomized pair order

--- pages/api/create-new-turn.ts
+++ pages/api/create-new-turn.ts
@@ -3,6 +3,7 @@
 import { sql } from '@vercel/postgres';
 import { NextApiResponse, NextApiRequest } from 'next';
 
+const MAX_PAIR_ID = 50;
 export default async function handler(
   request: NextApiRequest,
   response: NextApiResponse,
@@ -10,27 +11,37 @@ export default async function handler(
   const gameId = request.query.gameId as string;
   if (!gameId) throw new Error('Missing gameId');
   let pairToReturn = -1;
+  let currTurn = null;
 
   try {
     const { rows } = await sql`SELECT * FROM turns t where t.game_id = ${gameId};`;
-    let currTurn = null;
-    let maxPair = -1;
+    const pairsUsed = new Set<number>();
 
     for (const turn of rows) {
       if (turn.completed_at == null) {
         currTurn = turn;
         pairToReturn = currTurn.pair_id;
-      } else if (turn.pair_id > maxPair) {
-        maxPair = turn.pair_id;
+      } else {
+        pairsUsed.add(turn.pair_id);
       }
     }
     if (currTurn == null) {
-      await sql`INSERT INTO turns (game_id, pair_id) VALUES (${gameId}, ${Math.max(1, maxPair + 1)});`;
-      pairToReturn = Math.max(1, maxPair + 1);
-      const { rows } = await sql`
-        SELECT * FROM turns t
-        where t.game_id = ${gameId} and t.pair_id = ${pairToReturn};`;
-      await sql`INSERT INTO submissions (turn_id, counter) VALUES (${rows[0]?.id}, 0);`;
+      // Get random unused pair ID
+      const availablePairs = Array.from(
+        { length: MAX_PAIR_ID },
+        (_, i) => i + 1
+      ).filter(id => !pairsUsed.has(id));
+
+      if (availablePairs.length === 0) {
+        return response.status(400).json({ error: "No more available pairs" });
+      }
+
+      const randomIndex = Math.floor(Math.random() * availablePairs.length);
+      pairToReturn = availablePairs[randomIndex];;
+
+      const { rows: newTurn } = await sql`INSERT INTO turns (game_id, pair_id) VALUES (${gameId}, ${pairToReturn}) RETURNING *;`;
+      currTurn = newTurn[0];
+      await sql`INSERT INTO submissions (turn_id, counter) VALUES (${newTurn[0]?.id}, 0);`;
     }
 
   } catch (error) {
@@ -52,7 +63,7 @@ export default async function handler(
     FROM turns t 
     JOIN pairs p on t.pair_id = p.id 
     LEFT JOIN submissions s on t.id = s.turn_id
-    where t.game_id = ${gameId} and t.pair_id=${pairToReturn};`;
+    where t.id = ${currTurn?.id};`;
   console.log("gameID: ", gameId, "pairToReturn: ", pairToReturn);
   // if (rows.length == 0) {
   //   return response.status(500).json({ error: "error finding turn I just created" });
