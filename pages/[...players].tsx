4092f00ab6e502aaf97e484feb5ff9f7e5a6c462
Potluck Mittal <potluck.mittal@gmail.com>
2025-01-15 21:27:09 -0500

    even more logging

--- pages/[...players].tsx
+++ pages/[...players].tsx
@@ -199,7 +199,7 @@ export default function Page() {
         }
       };
       eventSource.onerror = function (e) {
-        console.log("eventsource error pots: " + e.type + " " + eventSource.readyState);
+        console.log("eventsource error pots: " + e.type + " " + e);
       };
       return () => {
         eventSource.close();
