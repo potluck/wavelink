b580088cbd65eb95aa6c8b06f1215defcf129532
Potluck Mittal <potluck.mittal@gmail.com>
2025-01-24 12:35:19 -0500

    return single user

--- pages/api/create-or-retrieve-user.ts
+++ pages/api/create-or-retrieve-user.ts
@@ -25,7 +25,7 @@ export default async function handler(
     }
 
     const {rows: user} = await sql`INSERT INTO users (name, slug) VALUES (${userName}, ${slug}) returning *;`;
-    return response.status(200).json({ user, retrievedUser: false, userHasPasskey: false });
+    return response.status(200).json({ user: user[0], retrievedUser: false, userHasPasskey: false });
   } catch (error) {
     return response.status(500).json({ error });
   }
