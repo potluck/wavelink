d853dee8aa3c411ff79d98a18688b413123a158e
Potluck Mittal <potluck.mittal@gmail.com>
2025-01-25 18:34:08 -0500

    real fix for /name/name link

--- pages/[...players].tsx
+++ pages/[...players].tsx
@@ -456,6 +456,9 @@ export default function Page() {
   }
 
   if (players.length == 1 || (players.length == 2 && players[0].toLowerCase() == players[1].toLowerCase())) {
+    if (players.length == 2 && players[0].toLowerCase() == players[1].toLowerCase()) {
+      router.push(`/${players[0]}`);
+    }
     return <Share player1={player1} gamesToRespondTo={gamesToRespondTo} userId1={userId1} />;
   }
 
