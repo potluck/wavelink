2674adab5118fed3d2fd8bced031737b5fc90de0
Potluck Mittal <potluck.mittal@gmail.com>
2025-01-26 15:36:19 -0500

    update /

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,26 @@
-import localFont from "next/font/local";
+import { Nunito } from 'next/font/google'
+import Link from "next/link";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const nunito = Nunito({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
+      className={`${nunito.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20`}
     >
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <ul className="list-inside text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li>Hey! You&apos;ve reached the homepage for Wavelink. This game is still in the Alpha phase of development.</li>
+        <ul className="list-inside text-base sm:text-base text-center sm:text-left">
+          <li>Hey! You&apos;ve reached the homepage for Wavelink. This game is in the Beta phase of development.</li>
           <br />
-          <li>Reach out to Potluck for an invite if you want to play!</li>
+          <li>You can start a game with Potluck by clicking the button below!</li>
         </ul>
+        <Link href="/potluck/invite" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Start a game with Potluck
+        </Link>
       </main>
+      <footer className="row-start-3 text-base">
+        <Link href="/help" className="text-blue-600 hover:text-blue-800 underline">How to play</Link>
+      </footer>
     </div>
   );
 }
