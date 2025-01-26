66bcd1fefb6b4f9633489d1b7999488c3e6df84a
Potluck Mittal <potluck.mittal@gmail.com>
2025-01-26 15:40:08 -0500

    dark mode for index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,20 +6,21 @@ const nunito = Nunito({ subsets: ['latin'] })
 export default function Home() {
   return (
     <div
-      className={`${nunito.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20`}
+      className={`${nunito.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 dark:bg-gray-900 dark:text-white`}
     >
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <ul className="list-inside text-base sm:text-base text-center sm:text-left">
+        <h1 className="text-4xl font-bold text-center w-full dark:text-white">Wavelink &nbsp;&nbsp;🌊&thinsp;🔗</h1>
+        <ul className="list-inside text-base sm:text-base text-center sm:text-left dark:text-gray-300">
           <li>Hey! You&apos;ve reached the homepage for Wavelink. This game is in the Beta phase of development.</li>
           <br />
           <li>You can start a game with Potluck by clicking the button below!</li>
         </ul>
-        <Link href="/potluck/invite" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <Link href="/potluck/invite" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded dark:bg-blue-600 dark:hover:bg-blue-800">
           Start a game with Potluck
         </Link>
       </main>
       <footer className="row-start-3 text-base">
-        <Link href="/help" className="text-blue-600 hover:text-blue-800 underline">How to play</Link>
+        <Link href="/help" className="text-blue-600 hover:text-blue-800 underline dark:text-blue-400 dark:hover:text-blue-300">How to play</Link>
       </footer>
     </div>
   );
