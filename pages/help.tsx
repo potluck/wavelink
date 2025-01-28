931cabd05efe326c2284ba0e9a4dc34af3b76381
Potluck Mittal <potluck.mittal@gmail.com>
2025-01-28 10:31:45 -0500

    back button on help page

--- pages/help.tsx
+++ pages/help.tsx
@@ -7,6 +7,12 @@ export default function Help() {
     <div className={`${nunito.className} max-w-2xl mx-auto p-6 space-y-6`}>
       <h1 className="text-4xl font-bold text-center w-full">Wavelink &nbsp;&nbsp;🌊&thinsp;🔗</h1>
       <Explainer />
+      <button
+        onClick={() => window.history.back()}
+        className="block mx-auto px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors"
+      >
+        ← Go Back
+      </button>
     </div>
   )
 }
