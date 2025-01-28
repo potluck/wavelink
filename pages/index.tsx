d4cdccaff8ebd217b87fae9c4ce29a64a8cf6bd7
Potluck Mittal <potluck.mittal@gmail.com>
2025-01-28 17:24:57 -0500

    more explainer to indexX

--- pages/index.tsx
+++ pages/index.tsx
@@ -92,6 +92,8 @@ export default function Home() {
         <ul className="list-inside text-base sm:text-base text-center sm:text-left dark:text-gray-300">
           <li>Hey! You&apos;ve reached the homepage for Wavelink. This game is in the Beta phase of development.</li>
           <br />
+          <li>Wavelink is a 2-player word association game. The rules will be explained once you start playing.</li>
+          <br />
           <li>You can start a game with Potluck or the AI partner by clicking the button below!</li>
           <br />
           <li>Or, create an account and send your invite link to a friend!</li>
