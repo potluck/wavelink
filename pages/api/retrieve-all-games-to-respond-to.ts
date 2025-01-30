5658ee150e270edc15c6d99cc01524554b8b3e48
Potluck Mittal <potluck.mittal@gmail.com>
2025-01-30 15:20:30 -0500

    more games to respond to

--- pages/api/retrieve-all-games-to-respond-to.ts
+++ pages/api/retrieve-all-games-to-respond-to.ts
@@ -6,15 +6,25 @@ export default async function handler(
   request: NextApiRequest,
   response: NextApiResponse,
 ) {
-
+  // TODO: include games where the last turn was completed and no new turn has been started
   const userId = request.query.userId as string;
   if (!userId) throw new Error('Missing userId');
   const { rows } = await sql`
     SELECT distinct g.id FROM games g 
     JOIN turns t on t.game_id = g.id and t.completed_at is null 
     JOIN submissions s on s.turn_id = t.id and s.completed_at is null
-    WHERE ((g.user_id1 = ${userId} and s.link1 is null and s.link2 is not null)
-      OR (g.user_id2 = ${userId} and s.link2 is null and s.link1 is not null)
-    );`;
+    WHERE ((g.user_id1 = ${userId} and s.link1 is null)
+      OR (g.user_id2 = ${userId} and s.link2 is null)
+    )
+    UNION ALL
+    SELECT distinct g.id FROM games g
+    JOIN turns t on t.game_id = g.id and t.completed_at is not null
+    WHERE ((g.user_id1 = ${userId} or g.user_id2 = ${userId})) AND NOT EXISTS (
+      SELECT 1
+      FROM turns AS t
+      WHERE t.game_id = g.id
+      AND t.completed_at IS NULL
+    )
+    ;`;
   return response.status(200).json({ rows });
 }
