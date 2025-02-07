72e8eef7a0d9f34b8e0402b402e493b1791f39ab
Potluck Mittal <potluck.mittal@gmail.com>
2025-02-07 15:09:21 -0500

    missing states

--- pages/components/PasskeySelector.tsx
+++ pages/components/PasskeySelector.tsx
@@ -105,6 +105,7 @@ export default function PasskeySelector({
               'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada',
               'New Hampshire', 'New Jersey', 'New Mexico', 'New York',
               'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon',
+              'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota',
               'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington',
               'West Virginia', 'Wisconsin', 'Wyoming']
               .map((state) => (
